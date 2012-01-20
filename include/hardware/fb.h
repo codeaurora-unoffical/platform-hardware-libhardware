9a699a92cabf49b154329017d70aa7fce05c07b1
Jeykumar Sankaran <jsanka@codeaurora.org>
2012-01-20 12:47:30 -0800

    Revert "Add interface for waiting for buffer display to be done."
    This reverts commit be5b79adaa92163db4436bd4e9163aa105c2972b.
    
    This change is not needed any more because we are caching the MDP pipe
    contents when closing the channel. So no need to wait for postFB finish
    
    Change-Id: Iec42988b5197a6f90e8c8c70662290a6d51a84fe

--- include/hardware/fb.h
+++ include/hardware/fb.h
@@ -151,8 +151,6 @@ typedef struct framebuffer_device_t {
     int (*enableHDMIOutput) (struct framebuffer_device_t* dev, int);
     int (*setActionSafeWidthRatio) (struct framebuffer_device_t* dev, float);
     int (*setActionSafeHeightRatio) (struct framebuffer_device_t* dev, float);
-    int (*resetBufferPostStatus)(struct framebuffer_device_t* dev);
-    int (*waitForBufferPost) (struct framebuffer_device_t* dev);
 
 } framebuffer_device_t;
 
