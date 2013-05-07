93226241c35eea5ae139c8370a6d8ad837c8106e
Stephen Li <stephenl@codeaurora.org>
2013-05-06 21:45:26 -0700

    gps: change position accuracy to float
    The accuracy returned from hybrid position can be very large, can
    exceed range of uint_16. Thus the prototype of HEPE is changed to float.
    
    Change-Id: Iaa52bd16db15333f08451e5addb484146c65ee0b

--- include/hardware/gps.h
+++ include/hardware/gps.h
@@ -646,7 +646,7 @@ typedef struct  {
     /** longitude in degrees */
     double longitude;
     /** Horzizontal error estimate in meters */
-    uint16_t HEPE;
+    float HEPE;
 } UlpNetworkPosition;
 
 /** Represents access point information */
