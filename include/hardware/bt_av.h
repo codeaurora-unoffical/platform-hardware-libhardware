1b98d6814c6924d40f48eaa0aa48c7a026e4185e
Venkateshwarlu Domakonda <vdomak@codeaurora.org>
2015-11-18 14:59:07 +0530

    Bluetooth: Remove A2dp soft hands-off support
    Removal of a2dp multicast feature from m-auto branch.
    
    Change-Id: I4a76b843d4c29832b04b8b8cf74f6e5f6556e229

--- include/hardware/bt_av.h
+++ include/hardware/bt_av.h
@@ -1,7 +1,6 @@
 /*
- * Copyright (C) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2013-2015, The Linux Foundation. All rights reserved.
  * Not a Contribution.
- *
  * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -50,11 +49,6 @@ typedef void (* btav_connection_state_callback)(btav_connection_state_t state,
 typedef void (* btav_audio_state_callback)(btav_audio_state_t state, 
                                                bt_bdaddr_t *bd_addr);
 
-/** Callback for connection priority of device for incoming connection
- * btav_connection_priority_t
- */
-typedef void (* btav_connection_priority_callback)(bt_bdaddr_t *bd_addr);
-
 /** Callback for audio configuration change.
  *  Used only for the A2DP sink interface.
  *  state will have one of the values from btav_audio_state_t
@@ -64,11 +58,6 @@ typedef void (* btav_connection_priority_callback)(bt_bdaddr_t *bd_addr);
 typedef void (* btav_audio_config_callback)(bt_bdaddr_t *bd_addr,
                                                 uint32_t sample_rate,
                                                 uint8_t channel_count);
-
-/** Callback for updating apps for A2dp multicast state.
- */
-
-typedef void (* btav_is_multicast_enabled_callback)(int state);
 
 /*
  * Callback for audio focus request to be used only in
@@ -84,8 +73,6 @@ typedef struct {
     btav_connection_state_callback  connection_state_cb;
     btav_audio_state_callback audio_state_cb;
     btav_audio_config_callback audio_config_cb;
-    btav_connection_priority_callback connection_priority_cb;
-    btav_is_multicast_enabled_callback multicast_state_cb;
     btav_audio_focus_request_callback audio_focus_request_cb;
 } btav_callbacks_t;
 
@@ -109,8 +96,7 @@ typedef struct {
     /**
      * Register the BtAv callbacks
      */
-    bt_status_t (*init)( btav_callbacks_t* callbacks , int max_a2dp_connections,
-                        int a2dp_multicast_state);
+    bt_status_t (*init)( btav_callbacks_t* callbacks );
 
     /** connect to headset */
     bt_status_t (*connect)( bt_bdaddr_t *bd_addr );
@@ -120,9 +106,6 @@ typedef struct {
 
     /** Closes the interface. */
     void  (*cleanup)( void );
-
-    /** Send priority of device to stack*/
-    void (*allow_connection)( int is_valid , bt_bdaddr_t *bd_addr);
 
     /** Sends Audio Focus State. */
     void  (*audio_focus_state)( int focus_state );
