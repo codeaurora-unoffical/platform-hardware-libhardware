bd2b42011c7713d1daab28dd0b07c8eb340de4e2
AnilKumar Chimata <anilc@codeaurora.org>
2016-02-17 23:58:44 +0530

    keymaster: Remove boringssl flags
    Remove boringssl flags for keymaster v0.3 test application.
    
    Change-Id: I5889299da6b4209cd7fa9066ead3839dac7cfdd0

--- tests/keymaster/Android.mk
+++ tests/keymaster/Android.mk
@@ -6,18 +6,26 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
     keymaster_test.cpp
 
+# Note that "bionic" is needed because of stlport
+LOCAL_C_INCLUDES := \
+    bionic \
+    external/gtest/include \
+    external/openssl/include \
+    external/stlport/stlport
+
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
-    libboringcrypto \
-    libhardware \
+    libcrypto \
+    libstlport \
+    libhardware
+
+LOCAL_STATIC_LIBRARIES := \
+    libgtest \
+    libgtest_main
 
 LOCAL_MODULE := keymaster_test
 
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_C_INCLUDES += \
-	$(LOCAL_PATH)/include \
-	$(TOP)/external/boringssl/include 
-
-include $(BUILD_NATIVE_TEST)
+include $(BUILD_EXECUTABLE)
