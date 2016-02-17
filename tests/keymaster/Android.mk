5d7614f27872e174b5841f478d58bed3c3431cb4
AnilKumar Chimata <anilc@codeaurora.org>
2016-02-17 12:21:40 +0530

    keymaster: Update keymaster tests
    Update keymaster tests as per the new header files from M.
    
    Change-Id: I157379fc76dc7db53a6fd5b724251fd17e4d7075

--- tests/keymaster/Android.mk
+++ tests/keymaster/Android.mk
@@ -6,26 +6,18 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
     keymaster_test.cpp
 
-# Note that "bionic" is needed because of stlport
-LOCAL_C_INCLUDES := \
-    bionic \
-    external/gtest/include \
-    external/openssl/include \
-    external/stlport/stlport
-
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
-    libcrypto \
-    libstlport \
-    libhardware
-
-LOCAL_STATIC_LIBRARIES := \
-    libgtest \
-    libgtest_main
+    libboringcrypto \
+    libhardware \
 
 LOCAL_MODULE := keymaster_test
 
 LOCAL_MODULE_TAGS := tests
 
-include $(BUILD_EXECUTABLE)
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/include \
+	$(TOP)/external/boringssl/include 
+
+include $(BUILD_NATIVE_TEST)
